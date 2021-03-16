b96500a65dda5adeb78836fcbfa765de0ddd2f89
Paul Florence <perso@florencepaul.com>
2021-03-16 17:17:40 +0100

    Fix html base template to expose title

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/signin.html'), name="login"),
+    path('signin/', auth_views.LoginView.as_view(template_name='accounts/signin.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name="password_reset"),
     path('signup/', views.signup, name="signup")
