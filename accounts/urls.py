edf0a63b6bdc9113a298eaf1decb59f795e415b3
Paul Florence <perso@florencepaul.com>
2021-03-16 17:01:07 +0100

    Fix front end css for auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from . import views
 # noinspection Pylint
 app_name = "accounts"
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/auth.html'), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/signin.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name="password_reset"),
-    path('signup/', auth_views.LoginView.as_view(template_name='accounts/signup.html'), name="signup")
+    path('signup/', views.signup, name="signup")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+def signup(request):
+    """
+    Signup view implementation : either register is successful and redirect to the login view, or render the form html.
+    """
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("accounts:login"))
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {"form": form})
