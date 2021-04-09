91e8a59a2934358c347b3121ccb7a003bee2c439
Paul Florence <perso@florencepaul.com>
2021-04-09 17:46:56 +0200

    Add demo docker

---
+++ docker_entrypoint_demo.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$DATABASE_TYPE" = "postgresql" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+echo "Migrating djRDO..."
+python manage.py migrate
+echo "Done !"
+echo "Loading demo data ..."
+python manage.py loaddate demo.json
+echo "Done !"
+echo "Collecting static files ..."
+python manage.py collectstatic  --noinput
+echo "Done !"
+
+exec "$@"
